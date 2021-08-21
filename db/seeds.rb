1d629bcbce7c9d8d899cb9f7ee1aa84f81dc4fa9
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-08-22 01:55:07 +0200

    updated seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,11 +1,11 @@
 User.create(username: 'Nappa', email: 'nappa@dappa.com', password: '123456', first_name: 'Nappa', last_name: 'Freya', is_admin: true)
 User.create(username: 'Hoppie', email: 'hoppie@doppie.com', password: '123456', first_name: 'Hoppie', last_name: 'Willemse', is_admin: false)
-
 Agent.create(first_name: 'Joshua', last_name: 'Door', email: 'joshuadoor@gmail.com', number: '0987634391', picture: 'https://firebasestorage.googleapis.com/v0/b/tactus-caelesti.appspot.com/o/agents%2FP7JIqIE.png?alt=media&token=bfa90cd0-07e5-4b99-b15e-f678570049bd')
 Agent.create(first_name: 'Darlene (unisex name)', last_name: 'Memphis', email: 'darthememphis@gmail.com', number: '0987431489', picture: 'https://firebasestorage.googleapis.com/v0/b/tactus-caelesti.appspot.com/o/agents%2FEmbarrased_Man.jpg?alt=media&token=7168439b-a738-4122-96af-fba1dc158a9a')
 Agent.create(first_name: 'Yapi', last_name: 'Kobustus', email: 'yapikobus@gmail.com', number: '0874521321', picture: 'https://firebasestorage.googleapis.com/v0/b/tactus-caelesti.appspot.com/o/agents%2Fsalesman.jpg?alt=media&token=0ec04d27-2070-4673-a6d0-bc0eb34cb10b')
-
-Property.create(agent_id: 1, name: '6 Bed Mansion', description: 
+Property.create(agent_id: 1, name: '6 Bed Mansion', picture: 
+  'https://firebasestorage.googleapis.com/v0/b/tactus-caelesti.appspot.com/o/mansions%2Fmansion1.jpg?alt=media&token=af39a749-6fed-41f5-8ed5-d7e0fac7956c', 
+  description: 
   "Private retreat with exceptional sea and mountain views . View By Appointment Only
   
   Acquire your piece of paradise in prestigious mansion with this split four-level beach style home. The versatile accommodation with the most exquisite eco-pool set at the bottom of the garden overlooks the whole of Lasetto Bay with its own nature reserve and private walks down to the beach.
@@ -30,24 +30,38 @@ Property.create(agent_id: 1, name: '6 Bed Mansion', description:
   
   2 solar geysers
   
-  Fully alarmed and infrared beams", price: 10000000, address: '98 Parker Ave', floor_area: 650, land_area: 1511)
-Property.create(agent_id: 2, name: 'Videa Caelesti', description: 'A modern house', price: 10000, address: '98 Parker Ave', floor_area: 950, land_area: 1750)
-Property.create(agent_id: 2, name: 'Suganti Vapi', description: 'A mansion with the 3 doors', price: 150000, address: '98 Parker Ave', floor_area: 900, land_area: 2000)
+  Fully alarmed and infrared beams", price: 100000000, address: '98 Parker Ave', floor_area: 650, land_area: 1511)
+Property.create(agent_id: 2, name: 'Videa Casale', picture: 
+  'https://firebasestorage.googleapis.com/v0/b/tactus-caelesti.appspot.com/o/mansions%2Fmansion2.jpg?alt=media&token=cbce1309-7a4c-48c6-b4c3-06c733ce7500', description: 
+  'An evolution of Modernist Architecture designed with both purpose and intention to create a sublime experience of space and light. This Woods+Dangaren 8 bed 11 bath home + detached guest house is perched high above the Pacific Ocean in the highly coveted Upper Riviera. Majestic 180 coastline + city views are revealed through sliding walls of glass and framed openings to seamlessly connect indoor and outdoor spaces. The main floor introduces warm, organic materials to the experience with Duchateau flooring, custom casework, rift-oak paneling, and rough-hewn limestone to the living and dining areas. An open chefs kitchen with expansive views is complete with Miele appliances, Calacatta stone island, secondary kitchen, and open concept great room. Upstairs, beams of daylight diffuse through the transparent 3 story staircase as kinetic art. Skylights and glass envelop 3 ocean-view bedrooms and a primary suite with floors of radiant heat, fireplace, and pocketing Fleetwood doors that expose a uniquely private and serene outdoor deck, blurring the line between indoors and out. The lower infinity pool + grand spa level is complete with full kitchen, reflecting pond, 2 ensuite bedrooms, gym, office, and theater. The glass sliders from each room provide access to an outdoor environment that is quietly luxurious, elegantly fluid, and ideal for entertaining.', price: 26000000, address: '21 Jonta Str', 
+  floor_area: 750, land_area: 1031)
+Property.create(agent_id: 2, name: 'Suganti Vapi', picture: 
+  'https://firebasestorage.googleapis.com/v0/b/tactus-caelesti.appspot.com/o/mansions%2Fmansion3.jpg?alt=media&token=f86b9aac-574c-44f7-b9a8-67fcc19e04ac', description: 
+  '"California Dreaming" Not your typical party house in the Bird Streets.This rare two-story, 12,237 square foot Modern works beautifully as a full-time residence with seven bedrooms and 12 baths. Panoramic views are framed by mature landscape. Lush gardens with large infinity edge pool. Complete indoor-outdoor lifestyle.Amenities include: theater, walk-in wine cellar, a full wellness retreat with resistance pool, plunge pool, dry sauna and gym/massage room. Complete with smart home technology and all the amenities one could imagine. Minutes to Sunset Plaza and the finest stores of Beverly Hills. Excellent value in a most prime location.', 
+  price: 35000000, address: '27 Hephner Ave', floor_area: 1136, land_area: 1325)
+Property.create(agent_id: 3, name: 'Modern Trophy Estate', picture: 
+  'https://firebasestorage.googleapis.com/v0/b/tactus-caelesti.appspot.com/o/mansions%2Fmansion4.jpg?alt=media&token=b350a3e3-2b06-499f-9ae3-05666c36e67c', description: 
+  "Perched at the end of a quiet cul-de-sac stands this custom modern trophy estate.Stunning views of DTLA, the coastline, and Crestwood Hills from every angle, as if you are floating on air. Designed around the art of entertaining, floor to ceiling glass panels seamlessly disappear and integrate the view, the outdoors, the wrap around deck and the interior space. This private oasis is truly a must see in person to experience the energy and privacy.A modern floor plan that features a large sleek fully equipped chef's kitchen, an oversized dining room and an enormous living room that overlooks the captivating views. The main suite rivals any 5-star hotel, with an oversized walk-in closet and spa like bathroom. The lower level consists of a gym, family room, glass wine room, and movie theater.Featuring five en-suite bedrooms and 8 baths laid out over 7,040 square feet, a dramatic infinity pool, grassy pad and tons of space to bask in the sun and enjoy the So Cal weather. The ultimate modern view home.", 
+  price: 2500000, address: '4 Seaview Ave', floor_area: 650, land_area: 735)
 
 Room.create(property_id: 1, name: 'Bedrooms', amount: 6)
 Room.create(property_id: 1, name: 'Kitchens', amount: 2)
 Room.create(property_id: 1, name: 'Lounges', amount: 2)
 Room.create(property_id: 1, name: 'Bathrooms', amount: 6)
 Room.create(property_id: 1, name: 'Garages', amount: 2)
-Room.create(property_id: 2, name: 'Bedroom', amount: 4)
-Room.create(property_id: 2, name: 'Kitchen', amount: 2)
-Room.create(property_id: 2, name: 'Lounge', amount: 3)
-Room.create(property_id: 2, name: 'Bathroom', amount: 4)
-Room.create(property_id: 3, name: 'Bedroom', amount: 4)
-Room.create(property_id: 3, name: 'Kitchen', amount: 3)
-Room.create(property_id: 3, name: 'Lounge', amount: 1)
-Room.create(property_id: 3, name: 'Bathroom', amount: 4)
-
+Room.create(property_id: 2, name: 'Bedrooms', amount: 8)
+Room.create(property_id: 2, name: 'Kitchen', amount: 1)
+Room.create(property_id: 2, name: 'Lounges', amount: 1)
+Room.create(property_id: 2, name: 'Bathrooms', amount: 11)
+Room.create(property_id: 3, name: 'Bedrooms', amount: 7)
+Room.create(property_id: 3, name: 'Kitchens', amount: 1)
+Room.create(property_id: 3, name: 'Lounges', amount: 2)
+Room.create(property_id: 3, name: 'Saunas', amount: 1)
+Room.create(property_id: 3, name: 'Bathrooms', amount: 12)
+Room.create(property_id: 4, name: 'Bedrooms', amount: 5)
+Room.create(property_id: 4, name: 'Kitchens', amount: 1)
+Room.create(property_id: 4, name: 'Lounges', amount: 1)
+Room.create(property_id: 4, name: 'Bathrooms', amount: 8)
 Appointment.create(user_id: 1, property_id: 1, date: DateTime.now )
 Appointment.create(user_id: 1, property_id: 2, date: DateTime.now )
 Appointment.create(user_id: 2, property_id: 2, date: DateTime.now )
