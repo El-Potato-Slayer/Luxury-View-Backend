0c71d674ae566eb1219a375c856cc347acfdc24d
El-Potato-Slayer <leighchad.cl@gmail.com>
2021-09-04 16:20:59 +0200

    fixed linter errors

--- app/models/property.rb
+++ app/models/property.rb
@@ -4,7 +4,8 @@ class Property < ApplicationRecord
   has_many :rooms
   validates :name, :description, :picture, :address, presence: true, length: { minimum: 3 }
   validates :price, :floor_area, :land_area, presence: true
-  scope :shortened_attributes, lambda {
-                                 select(Property.attribute_names - %w[description floor_area land_area created_at updated_at])
-                               }
+  scope :shortened_attributes,
+        lambda {
+          select(Property.attribute_names - %w[description floor_area land_area created_at updated_at])
+        }
 end
